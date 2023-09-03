46b69ae287adf7eb94e707b6f66f315214995cb7
varun <varunannepu123@gmail.com>
2023-09-03 10:20:58 +0530

    roboshop scripts- non-functional requirement

--- catalogue.sh
+++ catalogue.sh
@@ -1,39 +1,39 @@
-echo ">>>>>>>>>>>>>>>> Create catalogue service <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>> Create catalogue service <<<<<<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo ">>>>>>>>>>>>>>>> Create mongodb repo <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Create mongodb repo <<<<<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo ">>>>>>>>>>>>>>>> Install nodejs repo <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Install nodejs repo <<<<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo ">>>>>>>>>>>>>>>> installing nodejs <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> installing nodejs <<<<<<<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo ">>>>>>>>>>>>>>>> Create application user <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Create application user <<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo ">>>>>>>>>>>>>>>> Create application directory <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Create application directory <<<<<<<<<<<<<<<\e[0m"
 mkdir /app
 
-echo ">>>>>>>>>>>>>>>> Download application content <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Download application content <<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo ">>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo ">>>>>>>>>>>>>>>> Download nodejs dependencies <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Download nodejs dependencies <<<<<<<<<<<<<<<\e[0m"
 npm install
 
-echo ">>>>>>>>>>>>>>>> Install mongo client <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Install mongo client <<<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo ">>>>>>>>>>>>>>>> Load catalogue schema <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Load catalogue schema <<<<<<<<<<<<<<<\e[0m"
 mongo --host mongodb.varundevops.online </app/schema/catalogue.js
 
-echo ">>>>>>>>>>>>>>>> Start catalogue service <<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Start catalogue service <<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
