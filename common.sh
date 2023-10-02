fdb1b14c32aacc59883bb556b2226bc3d073b672
varun <varunannepu123@gmail.com>
2023-10-03 00:33:06 +0530

    roboshop functions

--- common.sh
+++ common.sh
@@ -1,3 +1,29 @@
+func_appprequ() {
+   echo -e "\e[36m>>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<<<\e[0m"
+   useradd roboshop &>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>> cleanup existing application content <<<<<<<<<<<<<<<\e[0m"
+  rm -rf /app &>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>> create application directory <<<<<<<<<<<<<<<\e[0m"
+  mkdir /app &>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>> download application content <<<<<<<<<<<<<<<\e[0m"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<\e[0m"
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  cd /app
+}
+
+func_systemd() {
+  echo -e "\e[36m>>>>>>>>>>>>>>> start ${component} service<<<<<<<<<<<<<<<\e[0m"
+  systemctl daemon-reload
+  systemctl enable  ${component}
+  systemctl start  ${component}
+}
+
 func_nodejs() {
   log=/tmp/roboshop.log
 
@@ -13,26 +39,10 @@ func_nodejs() {
   echo -e "\e[36m>>>>>>>>>>>>>>> Install nodejs <<<<<<<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
 
-  echo -e "\e[36m>>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<<<\e[0m"
-  useradd roboshop &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>> remove application directory <<<<<<<<<<<<<<<\e[0m"
-  rm -rf /app &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>> create application directory <<<<<<<<<<<<<<<\e[0m"
-  mkdir /app &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>> download application content <<<<<<<<<<<<<<<\e[0m"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<\e[0m"
-  cd /app
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app
+func_appprequ
 
   echo -e "\e[36m>>>>>>>>>>>>>>> Install nodejs dependencies <<<<<<<<<<<<<<<\e[0m"
   npm install &>>${log}
-
 
   echo -e "\e[36m>>>>>>>>>>>>>>> Install mongo client <<<<<<<<<<<<<<<\e[0m"
   yum install mongodb-org-shell -y &>>${log}
@@ -40,38 +50,27 @@ func_nodejs() {
   echo -e "\e[36m>>>>>>>>>>>>>>> load user schema <<<<<<<<<<<<<<<\e[0m"
   mongo --host mongodb.varundevops.online </app/schema/${component}.js &>>${log}
 
-  echo -e "\e[36m>>>>>>>>>>>>>>> start user service <<<<<<<<<<<<<<<\e[0m"
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl restart ${component} &>>${log}
+func_systemd
 }
 
 func_java() {
-  echo -e "\e[36m>>>>>>>>>>>>>>> Create user service <<<<<<<<<<<<<<<\e[0m"
+  echo -e "\e[36m>>>>>>>>>>>>>>> Create ${component} service <<<<<<<<<<<<<<<\e[0m"
   cp shipping.service /etc/systemd/system/shipping.service
 
-  echo -e "\e[36m>>>>>>>>>>>>>>> Create user service <<<<<<<<<<<<<<<\e[0m"
+  echo -e "\e[36m>>>>>>>>>>>>>>> Install maven <<<<<<<<<<<<<<<\e[0m"
   yum install maven -y
 
-  echo -e "\e[36m>>>>>>>>>>>>>>> Create user service <<<<<<<<<<<<<<<\e[0m"
-  useradd roboshop
+func_appprequ
 
-  echo -e "\e[36m>>>>>>>>>>>>>>> Create user service <<<<<<<<<<<<<<<\e[0m"
-  mkdir /app
-
-  echo -e "\e[36m>>>>>>>>>>>>>>> Create user service <<<<<<<<<<<<<<<\e[0m"
-  curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-  cd /app
-  unzip /tmp/shipping.zip
-  cd /app
+  echo -e "\e[36m>>>>>>>>>>>>>>>  Build ${component} service <<<<<<<<<<<<<<<\e[0m"
   mvn clean package
-  mv target/shipping-1.0.jar shipping.jar
+  mv target/ ${component}-1.0.jar  ${component}.jar
 
+  echo -e "\e[36m>>>>>>>>>>>>>>>  Install mysql client <<<<<<<<<<<<<<<\e[0m"
   yum install mysql -y
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>  Load schema <<<<<<<<<<<<<<<\e[0m"
   mysql -h mysql.varundevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
-  systemctl daemon-reload
-  systemctl enable shipping
-  systemctl start shipping
-
+func_systemd
 }
