f9640614a98c0b53b0a20e95fb2747812b9c51cc
varun <varunannepu123@gmail.com>
2023-10-05 22:57:03 +0530

    password NFR

--- dispatch.sh
+++ dispatch.sh
@@ -1,18 +1,37 @@
+source common.sh
+
+echo -e "\e[36m>>>>>>>>>>>>>>> Install Nginx <<<<<<<<<<<<<<<\e[0m"
 cp dispatch.service /etc/systemd/system/dispatch.service
 
+
+
+echo -e "\e[36m>>>>>>>>>>>>>>> Install Nginx <<<<<<<<<<<<<<<\e[0m"
 yum install golang -y
+
+
+
+
 useradd roboshop
 mkdir /app
+
+
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
+
+
 unzip /tmp/dispatch.zip
+
+
 cd /app
+
+
 go mod init dispatch
 go get
 go build
 
+
+
 systemctl daemon-reload
 systemctl enable dispatch
-systemctl
-start dispatch
+systemctl start dispatch
 
